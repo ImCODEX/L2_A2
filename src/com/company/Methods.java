bb21a91856cf4a173e4525447a7ca0d4d18f9702
Razvan Postescu <razvanpostescu@gmail.com>
2021-10-14 15:22:51 +0300

    Added javadoc

--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -2,46 +2,56 @@ package com.company;
 
 public class Methods {
 
-    //in: int[]; out: int
-    //Computes maximum from a given array
+    /**
+     * Computes maximum from a given array
+     * @param array: array of numbers
+     * @return: maximum
+     */
     public int max_nr(int[] array){
         int max = 0;
-        for(int i=0; i<array.length; i++)
-            if(array[i] > max)
-                max = array[i];
+        for (int j : array)
+            if (j > max)
+                max = j;
         return max;
     }
 
-    //in: int[]; out: int
-    //Computes minimum from a given array
+    /**
+     * Computes minimum from a given array
+     * @param array: array of numbers
+     * @return: minimum
+     */
     public int min_nr(int[] array){
         int min = Integer.MAX_VALUE;
-        for(int i=0; i<array.length; i++)
-            if(array[i] < min)
-                min = array[i];
+        for (int j : array)
+            if (j < min)
+                min = j;
         return min;
     }
 
-    //in: int[]; out: int
-    //Computes maximum sum with n-1 elements from a given array
-    //where n = array.length()
+    /**
+     * Computes maximum sum with n-1 elements from a given array
+     * @param array: array of numbers
+     * @return: maximum sum
+     */
     public int max_sum(int[] array){
         int min = min_nr(array), sum=0;
-        for(int i=0; i<array.length; i++)
-            if(array[i] != min)
-                sum+=array[i];
+        for (int j : array)
+            if (j != min)
+                sum += j;
 
         return sum;
     }
 
-    //in: int[]; out: int
-    //Computes minimum sum with n-1 elements from a given array
-    //where n = array.length()
+    /**
+     * Computes minimum sum with n-1 elements from a given array
+     * @param array: array of numbers
+     * @return: minimum sum
+     */
     public int min_sum(int[] array){
         int max = max_nr(array), sum=0;
-        for(int i=0; i<array.length; i++)
-            if(array[i] != max)
-                sum+=array[i];
+        for (int j : array)
+            if (j != max)
+                sum += j;
 
         return sum;
     }
